c55525b8eeaa5a6671899022b1a8fd5d1f33b91d
puvvala vijayalakshmi <vijayalaxmi0703>
2026-05-24 21:40:51 +0530

    Day 4: Implement Deepgram speech-to-text API

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const fs = require("fs");
+const { Deepgram } = require("@deepgram/sdk");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
-/* Middleware */
 app.use(cors());
 app.use(express.json());
 
@@ -17,7 +18,22 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-/* Multer Storage Setup */
+/* Schema */
+const transcriptionSchema = new mongoose.Schema({
+  filename: String,
+  transcription: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Transcription = mongoose.model(
+  "Transcription",
+  transcriptionSchema
+);
+
+/* Multer Setup */
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
@@ -30,22 +46,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-/* MongoDB Schema */
-const transcriptionSchema = new mongoose.Schema({
-  filename: String,
-  transcription: String,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-/* MongoDB Model */
-const Transcription = mongoose.model(
-  "Transcription",
-  transcriptionSchema
-);
-
+/* Deepgram */
+const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY);
 /* Test Route */
 app.get("/", (req, res) => {
   res.send("Backend is running");
@@ -54,27 +56,48 @@ app.get("/", (req, res) => {
 /* Upload Route */
 app.post("/upload", upload.single("audio"), async (req, res) => {
   try {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "No audio file uploaded",
+      });
+    }
+
+    const audioBuffer = fs.readFileSync(req.file.path);
+
+    const response = await deepgram.transcription.preRecorded(
+  {
+    buffer: audioBuffer,
+    mimetype: "audio/mp3",
+  },
+  {
+    punctuate: true,
+    model: "nova",
+  }
+  );
+
+    const transcription =
+     response.results.channels[0].alternatives[0].transcript;
+
     const newTranscription = new Transcription({
       filename: req.file.filename,
-      transcription: "Sample transcription",
+      transcription,
     });
 
     await newTranscription.save();
 
     res.json({
-      message: "File uploaded and saved successfully",
-      data: newTranscription,
+      message: "Transcription successful",
+      transcription,
     });
   } catch (error) {
     console.log(error);
 
     res.status(500).json({
-      message: "Error uploading file",
+      message: "Error generating transcription",
     });
   }
 });
 
-/* Start Server */
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
